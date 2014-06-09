3e92018f2ddf417ba7c028e4c302733d2d3cc32d
charumathy <telliantportal@gmail.com>
2014-06-09 16:45:30 +0530

    Commit new files

---
+++ LeaveManagementSystem/LeaveManagementSystem/Models/EmployeeModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LeaveManagementSystem.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set;}
+
+        public string EmployeeFirstName { get; set; }
+
+        public string EmployeeLastName { get; set; }
+
+        public string YearsOfExperience { get; set; }
+
+        public string EmployeeDesignation { get; set; }
+
+        public string EmployeeRole { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime DateOfJoining { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PersonalEmailID { get; set; }
+
+        public string OfficialEmailID { get; set; }
+
+        public string Address { get; set; }
+
+
+    }
+}
---
+++ LeaveManagementSystem/LeaveManagementSystem/Models/UserAccountModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMatrix.WebData;
+
+namespace LeaveManagementSystem.Models
+{
+    public class UserAccountModel : ExtendedMembershipProvider
+    {
+        public override bool ValidateUser(string username, string password)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        
+        
+    }
+
+    public class RoleProvider : SimpleRoleProvider
+    {
+
+    }
+}
